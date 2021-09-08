d93e6681952ec6455a7139fea1bf5f15c3b1fc00
Brett Johnson <brejohns@redhat.com>
2021-09-08 11:02:27 +1000

    status change ok

--- undertow-statuschange/src/main/java/com/sdbrett/KubeApp.java
+++ undertow-statuschange/src/main/java/com/sdbrett/KubeApp.java
@@ -1,2 +1,23 @@
-package com.sdbrett;public class KubeApp {
+package com.sdbrett;
+
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class KubeApp {
+    static AtomicBoolean running = new AtomicBoolean(false);
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Define server");
+        Liveness server = new Liveness(8081);
+
+        server.allGoodInTheHood = new AtomicBoolean(true);
+
+        try{
+            Thread.sleep(10000);
+        } catch (Exception e){}
+
+        server.allGoodInTheHood = new AtomicBoolean(false);
+    }
 }
--- undertow-statuschange/src/main/java/com/sdbrett/Liveness.java
+++ undertow-statuschange/src/main/java/com/sdbrett/Liveness.java
@@ -1,2 +1,35 @@
-package com.sdbrett;public class Liveness {
+package com.sdbrett;
+
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.AllowedMethodsHandler;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.server.handlers.ResponseCodeHandler;
+import io.undertow.util.Headers;
+import io.undertow.util.Methods;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Liveness {
+
+    public AtomicBoolean allGoodInTheHood = new AtomicBoolean(false);
+
+    public Liveness(int port) {
+        PathHandler handler = new PathHandler(new ResponseCodeHandler(404));
+        handler.addExactPath("/liveness", new AllowedMethodsHandler(exchange -> {
+            if (allGoodInTheHood.get()) {
+                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                exchange.getResponseSender().send("Hello");
+            }
+            else {
+                new ResponseCodeHandler(500).handleRequest(exchange);
+            }
+
+        }, Methods.GET));
+
+        Undertow server = Undertow.builder().addHttpListener(port, "0.0.0.0").setHandler(handler).build();
+        server.start();
+    }
+
 }
